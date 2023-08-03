bf08c5b793977d021a2feb7ebcc72fe69336742e
vinicius berto quintino <vinicius_berto@hotmail.com.br>
2023-08-03 01:36:29 +0000

    Teste commit 3 Changes to be committed: 	deleted:    decisoes_simples.py

--- decisoes_simples.py
+++
@@ -1,9 +0,0 @@
-
-# DECISOES SIMPLES
-
-nome = input("Digite o nome: ")
-idade = int(input("Digite a idade: "))
-prioridade = "NÃO"
-if idade >= 65:
-    prioridade = "SIM"
-print("O paciente " + nome + " possui atendimento prioritário? " + prioridade)
