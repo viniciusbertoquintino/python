a81f9444d2e32b1a6e4c0a103f1dfe85e3331bc2
vinicius berto quintino <vinicius_berto@hotmail.com.br>
2023-08-03 01:42:19 +0000

    Verion 1.0 Changes to be committed: 	new file:   Capitulo2-Decisoes/decisoes.py

---
+++ Capitulo2-Decisoes/decisoes.py
@@ -0,0 +1,8 @@
+# DECISOES SIMPLES
+
+nome = input("Digite o nome: ")
+idade = int(input("Digite a idade: "))
+prioridade = "NÃO"
+if idade >= 65:
+    prioridade = "SIM"
+print("O paciente " + nome + " possui atendimento prioritário? " + prioridade)
