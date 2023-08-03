f7757edae0b8c9288faa555ddd1f1674d81b4f24
vinicius berto quintino <vinicius_berto@hotmail.com.br>
2023-08-03 01:26:29 +0000

    Decisões simples em python curso FIAP Changes to be committed: 	new file:   decisoes_simples.py

---
+++ decisoes_simples.py
@@ -0,0 +1,9 @@
+
+# DECISOES SIMPLES
+
+nome = input("Digite o nome: ")
+idade = int(input("Digite a idade: "))
+prioridade = "NÃO"
+if idade >= 65:
+    prioridade = "SIM"
+print("O paciente " + nome + " possui atendimento prioritário? " + prioridade)
